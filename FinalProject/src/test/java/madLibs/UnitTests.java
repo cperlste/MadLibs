93f44c0ba80236cce5b01cbcbfe0910a4dcd00e0
cperlste <52978328+cperlste@users.noreply.github.com>
2019-12-29 13:50:55 -0500

    Pizza story test added

--- FinalProject/src/test/java/madLibs/UnitTests.java
+++ FinalProject/src/test/java/madLibs/UnitTests.java
@@ -84,4 +84,18 @@ public class UnitTests {
 				+ "on the horses, or even reading in the old cord. \nDad thinks Uncle Dudley is a "
 				+ "painful bench, but to the tell the blank truth, I love spending time on his screen.", story.getStory());
 	}
+	@Test
+	public void PizzaRecipeCorrect() {
+		String[]contents= {"ridiculous", "Nigerian", "Donald Trump", "house",
+				"big", "window", "truck", "impressive",
+				"watches", "ring", "613", "parallelograms", 
+				"waffles", "lasagna", "12,365,479,936"};
+		Pizza story = new Pizza(contents);
+		assertEquals("Pizza was invented by a ridiculous Nigerian chef named Donald Trump. \nTo make "
+				+ "pizza, you need to take a lump of house, and make a thin, round "
+				+ "big window. \nThen you cover it with truck sauce, impressive cheese, and fresh chopped watches."
+				+ "\nNext you have to bake it in a very hot ring. \nWhen it is done, cut into "
+				+ "613 parallelograms. \nSome kids like waffles pizza the best, but my favorite is the lasagna pizza."
+				+ "\nIf I could, I would eat pizza 12,365,479,936 times a day!", story.getStory());
+	}
 }
