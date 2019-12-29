8f58d4a181d0fdb8713170a7a0ef6de18d751b7b
cperlste <52978328+cperlste@users.noreply.github.com>
2019-12-29 13:59:33 -0500

    LetterFromCamp test added

--- FinalProject/src/test/java/madLibs/UnitTests.java
+++ FinalProject/src/test/java/madLibs/UnitTests.java
@@ -98,4 +98,23 @@ public class UnitTests {
 				+ "613 parallelograms. \nSome kids like waffles pizza the best, but my favorite is the lasagna pizza."
 				+ "\nIf I could, I would eat pizza 12,365,479,936 times a day!", story.getStory());
 	}
+
+	@Test
+	public void LetterFromCampCorrect() {
+		String contents[]= {"instruments", "blankets", "Batsheva Tanenbaum", "nice",
+				"guitar", "purple", "run", "roof","barbeques", "keyboard",
+				 "lawns", "basketball","pools", "chicken", "lonely",
+				"trees", "elbow", "game", "Mindy"};
+		LetterFromCamp story= new LetterFromCamp(contents);
+		assertEquals("Dear Folks,\nCamp is great! I like all the instruments in my tent.I have become close as\n"+
+				 "two blankets in a pod with Batsheva Tanenbaum, who has a/an nice personality and is never without a\n"+
+				 "guitar. He/she tells really purple stories which make all of us run out loud. I have\n"+
+				 "to stop writing now. I know I promised a long roof, but this morning I washed my\n"+
+				 "shirts and barbeques and put them out to dry on the clothes keyboard and it looks likes it's\n"+
+				 "getting ready to rain cats and lawns. I better get off my basketball and get my pools off the\n"+
+				 "chicken line before I run out of lonely clothes. I promise to write a letter chock full of\n"+
+				 "trees before my elbow hits the pillow tonight...or tommorrow...or maybe I'll write Tuesday.\n"+
+				 "Your loving game,\nMindy", story.getStory());
+	}
+	
 }
