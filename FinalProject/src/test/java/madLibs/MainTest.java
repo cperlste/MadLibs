e9e4e8c57de76fb65f675b48d7d78a5b8a85511c
btanenba <53190638+btanenba@users.noreply.github.com>
2020-01-09 19:54:15 -0500

    classpath changed

--- FinalProject/src/test/java/madLibs/MainTest.java
+++ FinalProject/src/test/java/madLibs/MainTest.java
@@ -9,25 +9,25 @@ import org.mockito.Mockito;
 public class MainTest {
 	@Test
 	public void testGetChoice() {
-		Prompter prompter= Mockito.mock(Prompter.class);
+		Prompter prompter = Mockito.mock(Prompter.class);
 		Mockito.when(prompter.promptInt(Mockito.anyString())).thenReturn(1, 7);
-		Menu menu= new Menu(prompter);
-		int storyChoice= menu.getStoryChoice();
+		Menu menu = new Menu(prompter);
+		int storyChoice = menu.getStoryChoice();
 		assertEquals(1, storyChoice);
 	}
-	
-	@Test 
+
+	@Test
 	public void testStorySetup() {
-		String [] prompts = {"noun", "verb", "adjective", "plural noun", "adverb"};
-		Prompter prompter=Mockito.mock(Prompter.class);
+		String[] prompts = { "noun", "verb", "adjective", "plural noun", "adverb" };
+		Prompter prompter = Mockito.mock(Prompter.class);
 		Mockito.when(prompter.promptString("noun")).thenReturn("person");
 		Mockito.when(prompter.promptString("verb")).thenReturn("stop");
 		Mockito.when(prompter.promptString("adjective")).thenReturn("silly");
 		Mockito.when(prompter.promptString("plural noun")).thenReturn("files");
 		Mockito.when(prompter.promptString("adverb")).thenReturn("lazily");
-		Menu menu= new Menu(prompter);
-		String [] userInput = menu.storySetup(prompts);
-		assertArrayEquals(prompts, userInput);
+		Menu menu = new Menu(prompter);
+		String[] userInput = menu.storySetup(prompts);
+		String[] array = { "person", "stop", "silly", "files", "lazily" };
+		assertArrayEquals(array, userInput);
 	}
 }
-
